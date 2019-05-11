c1034cba32ffce2a0ba285aa0fcae80b7ee5108a
Vic Shóstak <koddr.me@gmail.com>
2019-05-11 09:47:04 +0300

    Add size-limit by @ai

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2019 True web artisans https://1wa.co
  * http://opensource.org/licenses/MIT The MIT License (MIT)
  *
- * a2hs.js v0.4.2 at 10/05/2019
+ * a2hs.js v0.4.3 at 11/05/2019
  *
  * Add to Home Screen
  *
